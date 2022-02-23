dfad2ded63b76bc0acc7587e533b5344039d5434
Tyler Samay <tds9636@rit.edu>
2022-02-23 11:54:35 -0500

    uwu

---
+++ PE - Sorted List/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PE___Sorted_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new SortedList, save a reference to it as an ISortedCollection 
+            // and add some random values to it
+            const int MaxScore = 100;
+            ISortedCollection sortedData = new SortedList();
+
+            Random rng = new Random();
+            int numsToAdd = rng.Next(10);
+            for (int i = 0; i < numsToAdd; i++)
+            {
+                sortedData.Add(rng.Next(MaxScore + 1));
+            }
+
+            // Print a summary of the sorted collection
+            Console.WriteLine(
+                "The sorted collection has {0} elements and thus IsEmpty returns {1}:",
+                sortedData.Count,
+                sortedData.IsEmpty);
+            sortedData.Print();
+
+            Console.WriteLine(
+                "\nThe min and max are {0} and {1}.",
+                sortedData.Min(),
+                sortedData.Max());
+
+            // And test the other methods
+            sortedData.Add(42);
+            if (sortedData.Contains(42))
+            {
+                Console.WriteLine("\nWe also snuck the meaning of life.");
+            }
+
+            if (!sortedData.Contains(MaxScore))
+            {
+                Console.WriteLine("\nBut not the maximum possible score.");
+            }
+
+            Console.WriteLine("\nLet's sort multiples of 5.");
+            sortedData.Clear();
+            sortedData.Add(15);
+            sortedData.Add(20);
+            sortedData.Add(5);
+            sortedData.Add(10);
+            sortedData.Print();
+        }
+    }
+}
---
+++ PE - Sorted List/SortedList.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE___Sorted_List
+{
+    /// uses interface
+    class SortedList : ISortedCollection
+    {
+        private List<int> list = new List<int>();
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+        public bool IsEmpty
+        {
+            get
+            {
+                if (Count == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        /// adds the given new data to the collection
+        public void Add(int newData)
+        {
+            // track if data added yet
+            bool dataAdded = false;
+            // check each int in list
+            for (int i = 0; i < list.Count; i++)
+            {
+                // if int at i is less than the new data
+                if (newData < list[i])
+                {
+                    // insert new data at position i
+                    list.Insert(i, newData);
+                    // change check to true, breaks loop
+                    dataAdded = true;
+                    break;
+                }
+            }
+            // if new data not added, add to end
+            if (!dataAdded)
+            {
+                list.Add(newData);
+            }
+        }
+
+        /// check if list contains given data
+        public bool Contains(int data)
+        {
+            // check each int in list
+            foreach (int i in list)
+            {
+                // if int at index i equals data return true
+                if (i == data)
+                {
+                    return true;
+                }
+            }
+            // if int not found in list return false
+            return false;
+        }
+
+        /// clears list
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        // prints each item in list
+        public void Print()
+        {
+            foreach (int i in list)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        /// returns lowest value in list
+        ///   or MinValue if the list is empty
+        public int Min()
+        {
+            if (IsEmpty)
+            {
+                return int.MinValue;
+            }
+            else
+            {
+                return list[0];
+            }
+        }
+
+        /// returns greatest value in list
+        ///   or MaxValue if the list is empty
+        public int Max()
+        {
+            if (IsEmpty)
+            {
+                return int.MaxValue;
+            }
+            else
+            {
+                return list[list.Count - 1];
+            }
+        }
+    }
+}
