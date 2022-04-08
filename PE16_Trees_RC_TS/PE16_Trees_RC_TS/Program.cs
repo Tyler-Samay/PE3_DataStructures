e7c2ca54dcff1b493ba9b2bd90c30ac3971f3853
Tyler Samay <tds9636@rit.edu>
2022-04-08 18:34:00 -0400

    PE 16

---
+++ PE16_Trees_RC_TS/PE16_Trees_RC_TS/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PE16_Trees_RC_TS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create TalentTreeNode Objects
+            TalentTreeNode magic = new TalentTreeNode("Magic", true);
+            TalentTreeNode fireball = new TalentTreeNode("Fireball", true);
+            TalentTreeNode crazyFireball = new TalentTreeNode("Crazy Big Fireball", false);
+            TalentTreeNode tinyFireballs = new TalentTreeNode("1000 Tiny Fireballs", true);
+            TalentTreeNode magicArrow = new TalentTreeNode("Magic Arrow", false);
+            TalentTreeNode iceArrow = new TalentTreeNode("Ice Arrow", false);
+            TalentTreeNode boomArrow = new TalentTreeNode("Exploding Arrow", false);
+            //Connect Nodes to each other
+            magic.Left = fireball;
+            magic.Right = magicArrow;
+            fireball.Left = crazyFireball;
+            fireball.Right = tinyFireballs;
+            magicArrow.Left = iceArrow;
+            magicArrow.Right = boomArrow;
+            //List all abilities
+            Console.WriteLine("--Listing all abilities in the game.--");
+            magic.ListAllAbilities();
+            Console.WriteLine();
+
+            //List all known abilities
+            Console.WriteLine("--Listing all my known abilities.--");
+            magic.ListKnownAbilities();
+            Console.WriteLine();
+
+            //List all possible abilities
+            Console.WriteLine("--Listing all abilities I could learn next.--");
+            magic.ListPossibleAbilities(magic);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ PE16_Trees_RC_TS/PE16_Trees_RC_TS/TalentTreeNode.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PE16_Trees_RC_TS
+{
+    class TalentTreeNode
+    {
+        //Fields
+        private string ability;
+        private bool learned;
+        private TalentTreeNode left;
+        private TalentTreeNode right;
+
+        //Properties
+        //Left Property
+        public TalentTreeNode Left
+        {
+            get { return left; }
+            set { left = value; }
+        }
+        //Right Property
+        public TalentTreeNode Right
+        {
+            get { return right; }
+            set { right = value; }
+        }
+
+        //Constructor
+        public TalentTreeNode(string ability, bool learned)
+        {
+            this.ability = ability;
+            this.learned = learned;
+        }
+
+        //Methods
+        //ListAllAbilities Method
+        public void ListAllAbilities()
+        {
+            if(this.left != null)
+            {
+                this.left.ListAllAbilities();
+            }
+            Console.WriteLine(this.ability);
+            if(this.right != null)
+            {
+                this.right.ListAllAbilities();
+            }
+        }
+        //ListKnownAbilities Method
+        public void ListKnownAbilities()
+        {
+            if (this.learned == true)
+            {
+                Console.WriteLine("Known Ability: " + this.ability);
+            }
+            if (this.left != null)
+            {
+                this.left.ListKnownAbilities();
+            }
+            if (this.right != null)
+            {
+                this.right.ListKnownAbilities();
+            }
+        }
+        //ListPossibleAbilities Method
+        public void ListPossibleAbilities(TalentTreeNode parent)
+        {
+            if(this.learned == false)
+            {
+                Console.WriteLine("Possible Ability: " + this.ability);
+            }
+            if(this.left != null && this.learned == true)
+            {
+                this.left.ListPossibleAbilities(parent);
+            }
+            if(this.right != null && this.learned == true)
+            {
+                this.right.ListPossibleAbilities(parent);
+            }
+        }
+    }
+}
